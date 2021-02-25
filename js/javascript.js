efd13fa9e876a09ae65bff96c4945c38b7148f07
RoWW4 <rociobeato91@gmail.com>
2021-02-25 11:17:56 +0100

    Mensaje verde y rojo

--- js/javascript.js
+++ js/javascript.js
@@ -1,29 +1,46 @@
 function CalculoDNI()
 {
     var dni = document.getElementById("dni").value;
-    /*alert(dni.toUpperCase());*/
     var letra = document.getElementById("letra").value;
-    var resto = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B",
+
+    var restoArray = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B",
                  "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"];
+    
+    //Cálculo de la letra del DNI
+    var calculo = restoArray[dni % 23];
+    var calculoLetra = document.getElementById("resultadoLetra");
+
+    //Si no coincide letra DNI
+    if(calculo != letra.toUpperCase()){
+        calculoLetra.innerHTML = "Error. La letra correcta es " + calculo;
+        calculoLetra.style.color = "red";
+    }
+
+    else{
+        calculoLetra.innerHTML = "Correcto. La letra es " + calculo;
+        calculoLetra.style.color = "green";
+    }
 
     //Control de validación de las entradas del usuario
-    if(dni == "" && letra == "")
+
+    if(dni == "" || letra == "")
     {
         alert("No puede haber campos vacíos");
+        break;
     }
 
     else if(isNaN(dni))
     {
         alert("DNI tiene que ser una cadena de números");
         document.getElementById("dni").value = "";
+        break;
     }
 
     else if(isNaN(letra) == false)
     {
         alert("Introduzca una letra.");
         document.getElementById("letra").value = "";
+        break;
     }
-
-
 
 }
