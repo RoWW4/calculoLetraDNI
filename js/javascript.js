fe10911ea730cd2202842074d5561240ba529803
RoWW4 <rociobeato91@gmail.com>
2021-02-27 13:57:55 +0100

    Practica OK

--- js/javascript.js
+++ js/javascript.js
@@ -1,46 +1,54 @@
 function CalculoDNI()
 {
+    //Recogida de datos
     var dni = document.getElementById("dni").value;
     var letra = document.getElementById("letra").value;
 
     var restoArray = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B",
                  "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"];
     
-    //Cálculo de la letra del DNI
-    var calculo = restoArray[dni % 23];
-    var calculoLetra = document.getElementById("resultadoLetra");
-
-    //Si no coincide letra DNI
-    if(calculo != letra.toUpperCase()){
-        calculoLetra.innerHTML = "Error. La letra correcta es " + calculo;
-        calculoLetra.style.color = "red";
-    }
-
-    else{
-        calculoLetra.innerHTML = "Correcto. La letra es " + calculo;
-        calculoLetra.style.color = "green";
-    }
-
     //Control de validación de las entradas del usuario
-
     if(dni == "" || letra == "")
     {
         alert("No puede haber campos vacíos");
-        break;
     }
 
     else if(isNaN(dni))
     {
         alert("DNI tiene que ser una cadena de números");
         document.getElementById("dni").value = "";
-        break;
     }
 
     else if(isNaN(letra) == false)
     {
         alert("Introduzca una letra.");
         document.getElementById("letra").value = "";
-        break;
     }
 
+    else
+    {
+        //Cálculo de la letra del DNI
+        var calculo = restoArray[dni % 23];
+        var mostrarLetra = document.getElementById("mostrarLetra");
+        mostrarLetra.innerHTML = "La letra del DNI es " + "<strong style=\"color:blue;\">"+ calculo + "</strong>";
+        var mostrarMensaje = document.getElementById("escribirMensaje");
+
+        //Si no coincide letra DNI
+        if(calculo != letra.toUpperCase()){
+            mostrarMensaje.innerHTML = "La letra introducida no es correcta.";
+            mostrarMensaje.style.color = "red";
+        }
+        //Si coincide letra DNI muestra mensaje
+        else{
+            mostrarMensaje.innerHTML = "La letra introducida es correcta.";
+            mostrarMensaje.style.color = "green";
+        }
+    }
+}
+
+function pulsarEnter(){
+    if(event.key == "Enter"){
+        CalculoDNI();
+    }
+    
 }
